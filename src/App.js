eda99cce971583820c10f92250f681afb8105137
anutagrebenyuk <107403620+anutagrebenyuk@users.noreply.github.com>
2022-09-05 20:41:36 +0300

    created React app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <a
+        className="btn btn-primary"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
+}
+
+export default App;
